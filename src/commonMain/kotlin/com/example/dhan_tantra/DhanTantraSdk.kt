d61aa8e8db8be347ca12ff0e921640343ea936cb
rahul.dey2 <rahul.dey2@flipkart.com>
2025-10-06 16:18:05 +0530

    add dhantantra submodule

---
+++ src/commonMain/kotlin/com/example/dhan_tantra/DhanTantraSdk.kt
@@ -0,0 +1,9 @@
+package com.example.dhan_tantra
+
+object DhanTantraSdk {
+    fun init(){}
+
+    fun isIapEnable(): Boolean{
+        return true
+    }
+}
--- src/commonMain/kotlin/com/example/dhan_tantra/Platform.kt
+++
@@ -1,3 +0,0 @@
-package com.example.dhan_tantra
-
-expect fun platform(): String
